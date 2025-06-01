a920b99c88852ddaf5b7e4eff21a80e355021580
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-06-01 14:07:27 -0400

    Update environment configuration: switch to production URLs for backend, services, frontend, and media; remove local development URLs.

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,14 @@
 export const environment = {
   production: false,
-  url_backend: 'http://127.0.0.1:8000/',
-  url_servicios: 'http://127.0.0.1:8000/api',
-  url_frontend: 'http://localhost:4200/',
-  url_media: 'http://127.0.0.1:8000/storage/',
+  // url_backend: 'http://127.0.0.1:8000/',
+  // url_servicios: 'http://127.0.0.1:8000/api',
+  // url_frontend: 'http://localhost:4200/',
+  // url_media: 'http://127.0.0.1:8000/storage/',
+
+  //remoto
+
+  url_backend: 'https://svcbmf.com/backend-api-colegio/',
+    url_servicios: 'https://svcbmf.com/backend-api-colegio/public/api',
+    url_frontend: 'https://malcolmcordova.com/app-padres-app/',
+    url_media: 'https://svcbmf.com/backend-api-colegio/storage/app/public/',
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: true,
-  url_backend: 'http://127.0.0.1:8000/',
-  url_servicios: 'http://127.0.0.1:8000/api',
-  url_frontend: 'http://localhost:4200/',
-  url_media: 'http://127.0.0.1:8000/storage/',
+  // url_backend: 'http://127.0.0.1:8000/',
+  // url_servicios: 'http://127.0.0.1:8000/api',
+  // url_frontend: 'http://localhost:4200/',
+  // url_media: 'http://127.0.0.1:8000/storage/',
 
   // remoto
-  // url_backend: 'https://malcolmcordova.com/lawyer-app/backend-api-lawyer/',
-  //   url_servicios: 'https://malcolmcordova.com/lawyer-app/backend-api-lawyer/public/api',
-  //   url_frontend: 'https://malcolmcordova.com/app-padres-app/',
-  //   url_media: 'https://malcolmcordova.com/lawyer-app/backend-api-lawyer/storage/app/public/',
+  url_backend: 'https://svcbmf.com/backend-api-colegio/',
+    url_servicios: 'https://svcbmf.com/backend-api-colegio/public/api',
+    url_frontend: 'https://malcolmcordova.com/app-padres-app/',
+    url_media: 'https://svcbmf.com/backend-api-colegio/storage/app/public/',
   
 };
 
